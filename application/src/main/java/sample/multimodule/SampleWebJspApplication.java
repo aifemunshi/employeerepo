de1565513ed663475fc4c42e1b1ba4a713c96464
aife munshi <aife.108044@gmail.com>
2021-02-09 22:40:03 +0530

    Buildspec path change

--- application/src/main/java/sample/multimodule/SampleWebJspApplication.java
+++ application/src/main/java/sample/multimodule/SampleWebJspApplication.java
@@ -2,8 +2,6 @@ package sample.multimodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * 
